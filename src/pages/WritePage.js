ec630c56171024ebd06bbd08017bead0b6ebc871
Gyumong <rlaalsrb3559@naver.com>
2020-05-15 17:27:46 +0900

    EditorContainer 만들기

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Editor from '../components/write/Editor';
+import EditorContainer from '../containers/write/EditorContainer';
 import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 const WritePage =()=>{
     return(
         <Responsive>
-            <Editor />
+            <EditorContainer />
             <TagBox />
             <WriteActionButtons />
         </Responsive>
